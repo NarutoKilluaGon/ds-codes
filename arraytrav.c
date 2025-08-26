f9865821be2571932d9f9881decc401b1c2a89d8
NarutoKilluaGon <devvatsa192005@gmail.com>
2025-08-26 19:30:36 +0530

    meow

---
+++ arraytrav.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number: ");
+   // 🔥 makes sure prompt is written immediately
+    scanf("%d", &n);
+    printf("%d\n", n);
+    return 0;
+}
